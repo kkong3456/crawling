fe6b801f208f498f5c705344d5b8fa85f0e040cc
kkong7 <kkongk@naver.com>
2023-08-16 15:44:03 +0900

    iphone_tracking

--- iphone_tracking/app.py
+++ iphone_tracking/app.py
@@ -5,6 +5,8 @@ import pandas as pd
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
+from wordcloud import WordCloud
+
 ssl._create_default_https_context = ssl._create_unverified_context
 
 chromedriver_autoinstaller.install()
@@ -37,11 +39,54 @@ time.sleep(1)
 input_element.send_keys(Keys.ENTER)
 time.sleep(2)
 
-before_h=broswer.execute_script('return window.scrollY')
-while True:
-    broswer.find_element(By.TAG_NAME,'body').send_keys(Keys.END)
+names=[]
+prices=[]
+links=[]
+reviews=[]
+
+for i in range(1,3):
+    url=f'https://search.shopping.naver.com/search/all?frm=NVSHATC&origQuery=%EC%95%84%EC%9D%B4%ED%8F%B013&pagingIndex={i}&pagingSize=40&productSet=total&query=%EC%95%84%EC%9D%B4%ED%8F%B013&sort=rel&timestamp=&viewType=list'
+    broswer.get(url)
+    time.sleep(2)
+    before_h=broswer.execute_script('return window.scrollY')
     
+    while True:
+        broswer.find_element(By.TAG_NAME,'body').send_keys(Keys.END)
+        time.sleep(1)
+        after_h=broswer.execute_script('return window.scrollY')
+        
+        if after_h==before_h:
+            break
+        
+        before_h=after_h
+        time.sleep(1)
+    
+    items=broswer.find_elements(By.CSS_SELECTOR,'.product_item__MDtDF')
 
+    for index,item in enumerate(items):
+        name=item.find_element(By.CSS_SELECTOR,'.product_title__Mmw2K').text
+        names.append(name) 
+        try:
+            price=item.find_element(By.CSS_SELECTOR,'.price_num__S2p_v').text
+        except:
+            price='판매중단'
+        prices.append(price)
+        
+        link=item.find_element(By.CSS_SELECTOR,'.product_title__Mmw2K > a').get_attribute('href')
+        links.append(link)
+        
+        try:
+            review=item.find_element(By.CSS_SELECTOR,'.product_etc_box__ElfVA > .product_etc__LGVaW > .product_num__fafe5').text
+        except Exception as e:
+            review='리뷰없음'
+        reviews.append(review)
 
+        print(index,name,price,link,review)
+    
+df=pd.DataFrame({'name':names,'price':prices,'link':links,'review':reviews})
 
-input()
+df.to_csv('아이폰13.csv')
+
+wc=WordCloud(font_path='./NanumGothic.ttf',colormap='PuBu',width=500,height=500)
+wc.generate(str(df['name']))
+wc.to_file('아이폰_워드크라우드.png')
